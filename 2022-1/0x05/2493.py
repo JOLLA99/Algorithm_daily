a3b2df34d439918852d878360d2c3a88e5f9f63f
JOLLA99 <yoon6148@daum.net>
2022-01-15 22:28:31 +0900

    20220115

---
+++ 2022-1/0x05/2493.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+tL = list(map(int, input().split()))
+aL = [0 for _ in range(n)]
+stack=[]
+
+for i in range(n+1, -1, -1):
+    if len(stack) == 0:
+        stack.append([i, tL[i]])
+    else:
+        while tL[i]>stack[len(stack)-1][1]:
+            tower = stack.pop()
+            aL.append[tower[0]] = i+1
+            if len(stack)==0:
+                break
+        
+        stack.append([i, tL[i]])
+
+for num in aL:
+    print(num, end=" ")
