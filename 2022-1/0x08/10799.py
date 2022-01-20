f383633a121bd5a6c57d1811252bd3338d2e937d
JOLLA99 <yoon6148@daum.net>
2022-01-20 22:00:56 +0900

    20220120

---
+++ 2022-1/0x08/10799.py
@@ -0,0 +1,20 @@
+import sys
+
+s = sys.stdin.readline().rstrip()
+stack = []
+result = 0 
+
+for i in range(len(s)):
+    
+    if s[i] == '(':
+        stack.append(s[i])
+    else:
+        if s[i-1] == '(':
+            stack.pop()
+            result += len(stack)
+        else:
+            stack.pop()
+            result +=1
+
+print(result)
+    
