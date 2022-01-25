fd6b2b16a51266dcf614ff54cb4df170c558c67a
JOLLA99 <yoon6148@daum.net>
2022-01-25 21:24:43 +0900

    20220125

---
+++ 2022-1/0x09/7569.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+M, N, H = map(int, input().split())
+queue = deque()
+graph = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
+
+def bfs():
+    while queue:
+        z,x,y = queue.popleft()
+
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
+
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if graph[i][j][k] == 1:
+                queue.append((i, j, k))
+
+bfs()
